5f2d907662baa328b193fb085a670d3452e0e074
inadF <fekete.dani99@gmail.com>
2022-09-19 20:42:30 +0200

    git javítása (előzőleg kommentként fel nem tüntetett hibáké is)

--- main.cpp
+++ main.cpp
@@ -4,21 +4,22 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //hiányzik egy alulvonás a vátozó névből
-    std::cout << '1-100 ertekek duplazasa' //nincs ; a sor végén
-    for (int i = 0;) // nem teljes a for ciklus
+    int *b = new int[N_ELEMENTS]; //hiányzik egy alulvonás a vátozó névből
+    std::cout << "1-100 ertekek duplazasa" ;//nincs ; a sor végén
+    for (int i = 0; i < N_ELEMENTS; i++) // nem teljes a for ciklus
     {
-        b[i] = i * 2;
+        b[i] = (i + 1) * 2;
     }
-    for (int i = 0; i; i++) //i  helyett i < N_ELEMENTS
+	std::cout << "Ertekek:";
+    for (int i = 0; i < N_ELEMENTS; i++) //i  helyett i < N_ELEMENTS
     {
-        std::cout << "Ertek:" // szintén hiányzik a ;
+        std::cout << b[i] << " "; // szintén hiányzik a ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] // hiányzik a ;
+        atlag += b[i]; // hiányzik a ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
