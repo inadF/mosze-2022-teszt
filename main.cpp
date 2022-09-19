d200623bf065dab023237ae1aa9df16791ccf84c
inadF <fekete.dani99@gmail.com>
2022-09-19 20:25:45 +0200

    main.cpp kommentelés

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //hiányzik egy alulvonás a vátozó névből
+    std::cout << '1-100 ertekek duplazasa' //nincs ; a sor végén
+    for (int i = 0;) // nem teljes a for ciklus
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //i  helyett i < N_ELEMENTS
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // szintén hiányzik a ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] // hiányzik a ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
